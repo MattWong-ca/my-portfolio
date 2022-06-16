f2ff86e5b9bca9134faa33ba682a4abdf4afdbaa
Matthew Wong <matthew.yt.wong@gmail.com>
2022-06-16 02:05:31 +0000

    Remove DataServlet dead code

--- portfolio/src/main/java/com/google/sps/servlets/DateServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DateServlet.java
@@ -1,25 +1,3 @@
-/*package com.google.sps.servlets;
-
-import java.io.IOException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
-@WebServlet("/date")
-public class DateServlet extends HttpServlet {
-
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("The server's current date is " + new Date());
-  }
-}
-public class DateServlet {
-    
-}*/
-
 package com.google.sps.servlets;
 
 import java.io.IOException;
