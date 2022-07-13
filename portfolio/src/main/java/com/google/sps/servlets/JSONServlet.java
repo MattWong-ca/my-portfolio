44dcce01ce8aac6b8c166e891e4418b023ad78fe
Matthew Wong <matthew.yt.wong@gmail.com>
2022-07-13 02:42:55 +0000

    Address PR comments

--- portfolio/src/main/java/com/google/sps/servlets/JSONServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/JSONServlet.java
@@ -13,26 +13,19 @@ import java.util.Arrays;
 @WebServlet("/JSONarray")
 public class JSONServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Initialize ArrayList and convert its type to JSON
-    ArrayList<String> jsonArray = new ArrayList<>(Arrays.asList(" JSON is cool", " GraphQL is cool", " Gson is cool"));
-    response.setContentType("application/json;");
+    private static final ArrayList<String> jsonArray = new ArrayList<>(Arrays.asList(
+        " JSON is cool", " GraphQL is cool", " Gson is cool"));
+    private static final Gson gson = new Gson();
 
-    // Uncomment + use this ArrayList once an athlete object is created (To be done)
-    // ArrayList<String> jsonArray = new ArrayList<>(Arrays.asList(
-    //         "{\"name\": \"Kobe Bryant\", \"jerseyNumber\": 8}",
-    //         "{\"name\": \"Michael Jordan\", \"jerseyNumber\": 23}",
-    //         "{\"name\": \"Steph Curry\", \"jerseyNumber\": 30}"));
-    
-    // Convert to JSON using Gson
-    Gson gson = new Gson();
-    String json = gson.toJson(jsonArray);
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Convert ArrayList type to JSON
+        response.setContentType("application/json;");
 
-    // Send the JSON as the response
-    response.getWriter().println(json);
+        // Convert to JSON using Gson
+        String json = gson.toJson(jsonArray);
 
-    // To be done later: create a proper redirect
-    // response.sendRedirect("link");
-  }
+        // Send the JSON as the response
+        response.getWriter().println(json);
+    }
 }
